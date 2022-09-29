9b47d962bb32fe6ce1f8d751c98086056624ec49
Joseth Flores Cabanillas <evadido12008@gmail.com>
2022-09-29 13:01:42 +0200

    Cambios funcionales v1

--- Poker_vue/src/store/utils/axios/index.js
+++ Poker_vue/src/store/utils/axios/index.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 import { isDevMode } from '../env.js';
 
 export function setupAxios(app) {
-  axios.defaults.baseURL = isDevMode() ? "http://localhost:3001/api/v1/" : "http://poker.boldworkplanner.com/";
+  axios.defaults.baseURL = isDevMode() ? "http://localhost:3001/api/v1/" : "https://poker.boldworkplanner.com/api/v1/";
 }
